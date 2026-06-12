26637db796c832e7021ce9c2797cf40b674c37d2
Priyanshu Kumar Shahi <kumarpriyanshu2910@gmail.com>
2026-06-12 09:57:28 +0530

    Adding two more code 1. Palindrome  , 2. Factorial

--- college js/index.js
+++ college js/index.js
@@ -10,22 +10,64 @@
 // else
 // {
 //     console.log("the number is Zero.");
-    
 // }
 
 
-let letter=prompt("Enter Your character.. :")
-let array=['a','e','i','o','u','A','E','I','O','U']
+// let letter=prompt("Enter Your character.. :")
+// let array=['a','e','i','o','u','A','E','I','O','U']
 
-if(letter.length==1){
-    if(array.includes(letter)){
-        console.log(`${letter} is a vowel.`);
-    }
-    else{
-        console.log(`${letter} is a consonant`);
+// if(letter.length==1){
+//     if(array.includes(letter)){
+//         console.log(`${letter} is a vowel.`);
+//     }
+//     else{
+//         console.log(`${letter} is a consonant`);
         
+//     }
+// }
+// else{
+//     console.log("Please Enter A single Character.")
+// }
+
+function isPalindrome(str) {
+
+    const cleanStr = str.toLowerCase();
+    
+    const reversedStr = cleanStr.split("").reverse().join("");
+    
+    return cleanStr === reversedStr;
+}
+
+const testWord1 = "Madam";
+const testWord2 = "Himanshu";
+
+console.log(`Is "${testWord1}" a Palindrome? -> ${isPalindrome(testWord1)}`); 
+console.log(`Is "${testWord2}" a Palindrome? -> ${isPalindrome(testWord2)}`);
+
+
+
+
+function calculateFactorial(num) {
+    
+    if (num < 0) {
+        return "Error: Negative numbers ka factorial nahi hota bhai!";
     }
+    
+    
+    if (num === 0 || num === 1) {
+        return 1;
+    }
+    
+    let result = 1;
+    
+    for (let i = 2; i <= num; i++) {
+        result *= i; 
+    }
+    
+    return result;
 }
-else{
-    console.log("Please Enter A single Character.")
-}
+
+
+const numberToTest = 5;
+console.log(`Factorial of ${numberToTest} is -> ${calculateFactorial(numberToTest)}`); 
+console.log(`Factorial of 0 is -> ${calculateFactorial(0)}`); 
